6deaca05743374e52a707dd383bda4503c2aef57
lyonsyonii <liam@garriga.dev>
2023-11-22 13:58:06 +0100

    Images will now be published in the ghcr.io repo

--- docker.sh
+++ docker.sh
@@ -1,10 +1,10 @@
-docker build -t lgarriga/rust-quest-runner .
+docker build -t ghcr.io/lyonsyonii/rust-quest-runner .
 
 case $1 in 
     run)
-        docker run -p 3030:3030 --cpus="2" lgarriga/rust-quest-runner;;
+        docker run -p 3030:3030 --cpus="2" ghcr.io/lyonsyonii/rust-quest-runner;;
     push)
-        docker push lgarriga/rust-quest-runner:latest;;
+        docker push ghcr.io/lyonsyonii/rust-quest-runner:latest;;
     *)
         echo "Usage: ./run_docker.sh [run|push]";;
 esac
--- project-template/src/main.rs
+++ project-template/src/main.rs
@@ -1,3 +1,5 @@
 #![forbid(unsafe_code)]
 
 extern crate std;
+
+// CODE GOES HERE
